f1c75d8bc39f5f89d1d9267e1ef8b1499a3ec855
Hrairi Mehdi <49190228+MEHDIHRAIRI@users.noreply.github.com>
2021-11-06 22:24:33 +0100

    Dockerfile

--- src/main/java/tn/esprit/spring/services/IEntrepriseService.java
+++ src/main/java/tn/esprit/spring/services/IEntrepriseService.java
@@ -5,7 +5,7 @@ import java.util.List;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Entreprise;
 
-public interface IEntrepriseService {
+public interface IEntrepriseService  {
 	
 	public int ajouterEntreprise(Entreprise entreprise);
 	public int ajouterDepartement(Departement dep);
